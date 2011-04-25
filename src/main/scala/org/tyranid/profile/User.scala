48368fe1c9f5901f91567e3850e990fa831a522a
dimensia <dimensia@tyranid.org>
2011-04-25 01:06:39 -0500

    work

---
+++ src/main/scala/org/tyranid/profile/User.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2008-2011 Tyranid <http://tyranid.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.tyranid.profile
+
+import net.liftweb.http.SessionVar
+
+object User {
+
+  private object currentVar extends SessionVar[Option[User]]( None )
+
+  def current:Option[User] = currentVar.is
+}
+
+class User {
+
+  /*
+
+      mongo object
+
+
+      
+
+      send an email to reset password
+
+   */
+
+}
+
