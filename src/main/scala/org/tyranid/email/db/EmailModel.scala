5917f2aa3c67f35fa84a68e868db8a6c5dacfd88
shindig <shindig@tyranid.org>
2011-05-23 18:13:21 -0500

    added tid

--- src/main/scala/org/tyranid/email/db/EmailModel.scala
+++ src/main/scala/org/tyranid/email/db/EmailModel.scala
@@ -3,7 +3,7 @@ package org.tyranid.email.db
 import org.tyranid.db.mongo.MongoEntity;
 import org.tyranid.db._;
 
-object EmailConfig extends MongoEntity {
+object EmailConfig extends MongoEntity( tid = "a0Kt" ) {
   "host"         is DbChar(40);
   "port"         is DbInt;
   "authUser"     is DbChar(40);
