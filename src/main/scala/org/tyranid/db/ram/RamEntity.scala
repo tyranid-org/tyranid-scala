037cf3ca8c43f16044a7d057124312bd460ec209
Dimensia <dimensia@tyranid.org>
2012-02-21 09:05:31 -0600

    added

--- src/main/scala/org/tyranid/db/ram/RamEntity.scala
+++ src/main/scala/org/tyranid/db/ram/RamEntity.scala
@@ -39,7 +39,18 @@ case class RamEntity( tid:String ) extends Entity {
     v
   }
 
-  def makeView:View = throw new UnsupportedOperationException
+  lazy val makeView:View = {
+    val v = new TupleView
+
+    val len = attribs.size
+    val vas = new Array[ViewAttribute]( len )
+
+    for ( i <- 0 until len )
+      vas( i ) = new ViewAttribute( v, attribs( i ), i )
+
+    v.leaves = vas
+    v
+  }
 
 	def create {}
 	def drop   { /* TODO */ }
