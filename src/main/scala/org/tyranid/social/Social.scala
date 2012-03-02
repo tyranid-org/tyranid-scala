21a8de7b347155cf2d843b60a4001968875fcf8b
dimensia <dimensia@tyranid.org>
2012-03-02 14:46:02 -0600

    work

--- src/main/scala/org/tyranid/social/Social.scala
+++ src/main/scala/org/tyranid/social/Social.scala
@@ -1,6 +1,9 @@
 
 package org.tyranid.social
 
+import org.bson.types.ObjectId
+import com.mongodb.DBObject
+
 import org.tyranid.Imp._
 import org.tyranid.profile.User
 import org.tyranid.social.linkedIn.LinkedIn
@@ -8,9 +11,11 @@ import org.tyranid.social.linkedIn.LinkedIn
 
 object Social {
 
+  // TODO:  this should return Org objects once we have Orgs in Tyranid
+  def createCompanies( fromOrgId:ObjectId, domains:Seq[String] ):Seq[DBObject] = {
+    //LinkedIn.createCompany( user, domain )
 
-  def createCompany( user:User, domain:String ) = {
-    LinkedIn.createCompany( user, domain )
+    Nil
   }
 }
 
