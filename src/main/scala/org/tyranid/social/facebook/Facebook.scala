97e313c5d81cc1e6768f2f901bc80169fc7f554e
dimensia <dimensia@tyranid.org>
2012-03-05 17:13:58 -0600

    work

--- src/main/scala/org/tyranid/social/facebook/Facebook.scala
+++ src/main/scala/org/tyranid/social/facebook/Facebook.scala
@@ -1,6 +1,8 @@
 
 package org.tyranid.social.facebook
 
+import scala.xml.Unparsed
+
 import org.tyranid.Imp._
 import org.tyranid.http.Http
 import org.tyranid.session.Session
@@ -9,6 +11,35 @@ import org.tyranid.web.{ Weblet, WebContext }
 
 
 object Facebook {
+
+}
+
+case class FbApp( apiKey:String, secret:String ) {
+
+  def script =
+    <div id="fb-root"></div>
+    <script>{ Unparsed( """
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '""" + apiKey + """',
+      channelUrl : '//""" + B.domain + """/facebook/channel',
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+""" ) }</script>
 }
 
 object Facebooklet extends Weblet {
