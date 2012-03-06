5e0a0b5c7607b7c36dc42388dd1739c025bfa500
shindig <shindig@tyranid.org>
2012-03-06 13:10:17 -0600

    fix

--- src/main/scala/org/tyranid/social/linkedin/LinkedIn.scala
+++ src/main/scala/org/tyranid/social/linkedin/LinkedIn.scala
@@ -257,7 +257,7 @@ case class LiApp( apiKey:String, secret:String ) {
 
   def createCompanies( fromOrgId:ObjectId, domains:Seq[String] ):Seq[Org] = {
 
-    val users = B.Org.db.find( Mobj( "org" -> fromOrgId, "liid" -> Mobj( $exists -> true ) ) ).toSeq
+    val users = B.User.db.find( Mobj( "org" -> fromOrgId, "liid" -> Mobj( $exists -> true ) ) ).toSeq
     var uIdx = 0
 
     domains.
