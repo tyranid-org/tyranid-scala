236c84722d61a40f560cd98c4db5b4524ceb7395
Dimensia <dimensia@tyranid.org>
2012-03-27 14:57:59 -0500

    work

--- src/main/scala/org/tyranid/social/Trackur.scala
+++ src/main/scala/org/tyranid/social/Trackur.scala
@@ -49,7 +49,43 @@ object Trackur extends MongoEntity( tid = "a0Jt" ) {
 
   def monitor = {
 
+    for ( query <- B.trackur.monitoredQueries().take( 3 ) ) {
 
+      val obj = db.findOrMake( Mobj( "query" -> query ) )
+
+val url = ( "http://api.trackur.com/index.php/api/json/" + B.trackur.apiKey + query )
+spam( "url=" + url )
+      val rslts = ( "http://api.trackur.com/index.php/api/json/" + B.trackur.apiKey + query ).GET().toJson
+
+      spam( "query=" + query )
+      spam( "rslts=" + rslts )
+
+      /*
+[ { "response": {
+      "link":"http://api.trackur.com/index.php/api/json/9a2c7879bf63799e3d2f71cc3b38a95e48f7/Dimensiology/1/1/////0/0",
+      "error":"No results found."
+    }
+  }
+]
+[ { "response": {
+      "link":"http://api.trackur.com/index.php/api/json/9a2c7879bf63799e3d2f71cc3b38a95e48f7/YRC/1/1/////0/0",
+      "user":{"id":"40106","requesttime":"1332906355","requestsused":"110","requestlimit":"100000","hourlyuse":"1"},
+      "request":{"keyword":"YRC","limit":"1","offset":"1","include":"","exclude":"","domains":"","sources":"","datestart":"0","dateend":"0"},
+      "totalresults":"541",
+      "results":[
+         {"id":"aa567fca1fb8106d","title":"Trucking Sector Gains Momentum -- YRC Worldwide and Arkansas Best Looking Strong - MarketWatch (press release)","source":"Twitter","published":"2:57am Mon Mar 26 2012","url":"http://twitter.com/TruckingMotYbum/statuses/184172081968185344","content":"Trucking Sector Gains Momentum -- YRC Worldwide and Arkansas Best Looking Strong - MarketWatch (press release)"}
+      ]
+    },
+    "navigation":{
+      "previous":"http://api.trackur.com/index.php/api/json/9a2c7879bf63799e3d2f71cc3b38a95e48f7/YRC/1/0/////0/0",
+      "next":"http://api.trackur.com/index.php/api/json/9a2c7879bf63799e3d2f71cc3b38a95e48f7/YRC/1/1/////0/0"
+    }
+  }
+]
+      */
+
+      db.save( obj )
+    }
   }
 }
 
