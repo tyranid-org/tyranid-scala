f516de6a4906aa24a10c7a7b0556bac1876d22c9
Dimensia <dimensia@tyranid.org>
2012-08-14 14:28:06 -0500

    work

--- src/main/scala/org/tyranid/content/Content.scala
+++ src/main/scala/org/tyranid/content/Content.scala
@@ -17,14 +17,19 @@
 
 package org.tyranid.content
 
+import scala.xml.Text
 
 import org.tyranid.Imp._
-import org.tyranid.db.{ DbChar, DbDateTime, DbInt, DbLink, EnumEntity }
-import org.tyranid.db.mongo.MongoEntity
+import org.tyranid.db.{ DbArray, DbBoolean, DbChar, DbDateTime, DbInt, DbLink, DbLong, DbTid, DbText, DbUrl, EnumEntity }
+import org.tyranid.db.es.{ SearchAuth, SearchText }
+import org.tyranid.db.mongo.{ DbMongoId, MongoEntity }
 import org.tyranid.db.ram.RamEntity
 import org.tyranid.db.tuple.Tuple
+import org.tyranid.profile.{ Group, Tag }
+import org.tyranid.secure.PrivateKeyEntity
 
 
+// TODO:  should this be in ui ?
 
 object ViewType extends RamEntity( tid = "a13v" ) with EnumEntity[ViewType] {
   "_id"    is DbInt      is 'id;
@@ -46,6 +51,7 @@ object ViewType extends RamEntity( tid = "a13v" ) with EnumEntity[ViewType] {
 }
 
 class ViewType extends Tuple( ViewType.makeView )
+
 
 object ContentType extends RamEntity( tid = "a10v" ) with EnumEntity[ContentType] {
   "_id"    is DbInt      is 'id;
@@ -71,6 +77,9 @@ class ContentType extends Tuple( ContentType.makeView )
 
 
 
+//
+// Replies
+//
 
 object Reply extends MongoEntity( tid = "b00w", embedded = true ) {
   "on"             is DbDateTime           ;
@@ -81,5 +90,66 @@ object Reply extends MongoEntity( tid = "b00w", embedded = true ) {
     "u"            is DbLink(B.User)       ;           // user who created the reply
   }
 
+}
+
+
+trait ContentMeta extends MongoEntity with PrivateKeyEntity {
+
+  "_id"               is DbMongoId            is 'id;
+
+  override def init = {
+    super.init
+
+  "on"                is DbDateTime           is 'required;
+  "type"              is DbLink(ContentType)  is 'required;
+  "tags"              is DbArray(DbLink(Tag)) ;
+  "name"              is DbChar(50)           is 'label is 'required is SearchText;
+
+  "o"                 is DbArray(DbTid(B.Org,B.User,Group)) as "Owners" is 'owner;
+  "v"                 is DbArray(DbTid(B.Org,B.User,Group)) as "Viewers" is SearchAuth;
+  "subV"              is DbArray(DbTid(B.Org,B.User))                   ; // for showing content inside a group
+
+  "lastModified"      is DbDateTime is 'required;
+  "lastModifiedBy"    is DbLink(B.User) is 'required;
+  "lastModifiedByOrg" is DbLink(B.Org) is 'required;
+
+  // Volees
+  "fit"               is DbBoolean            as "From in To" help <div>Indicates that the posting user is in the to list.  This will normally be false unless a user explicitly addresses a volee to themselves.</div>
+  "s3"                is DbBoolean            help Text( "Indicates that the file or img is stored by us in S3." );
+
+  "logTid"            is DbChar(64)           ; // TODO:  this is really a DbTid that contains an entity instead of a record
+  "logId"             is DbMongoId            ; // TODO:  this is a link to the "*_log" table for the above entity, model it better
+
+  "file"              is DbUrl /* TODO:  change to DbFile */        ; // if s3 this is a S3 path, otherwise it is an absolute URL
+  "fileMimeType"      is DbChar(64)           ;
+
+  "video"             is DbBoolean            ;
+
+
+  // Attachment / File
+  "fileName"          is DbChar(128)          is 'required;
+  "size"              is DbLong               ;
+  "desc"              is DbText               is SearchText;
+
+  // Messages
+  "m"                 is DbChar(1024)         is 'label is SearchText;
+  "r"                 is DbArray(Reply)       as "Replies";
+
+  // Image / Thumbnail
+  "img"               is DbUrl /* TODO:  change to DbImage? */;
+  "imgH"              is DbInt                help Text( "The actual height of the image." );
+  "imgW"              is DbInt                help Text( "The actual width of the image." );
+  
+  
+  // Attachment
+  "title"             is DbChar(128)          ;
+  "link"              is DbUrl                ;
+  "icon"              is DbUrl /* TODO:  should be DbImage ? */     ; // for links, this is the favicon
+  
+  // RSS & Atom Feeds
+  "feedOut"           is DbBoolean            help <span>If this is enabled, outgoing <b>public</b> RSS and Atom feeds are generated.</span>;
+  "feed"              is DbMongoId // should be DbLink(Feed)                               ;
+  "feedItemId"        is DbChar(128)          ;
+  }
 }
 
