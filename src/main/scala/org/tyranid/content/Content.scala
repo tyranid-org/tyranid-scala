b991457d2ad386825c3ffac53ab31780e337b8fc
shindig <shindig@tyranid.org>
2012-08-27 14:31:11 -0500

    fix

--- src/main/scala/org/tyranid/content/Content.scala
+++ src/main/scala/org/tyranid/content/Content.scala
@@ -305,8 +305,10 @@ abstract class Content( override val view:MongoView,
         
         try {
           f = Thumbnail.generate( imgFile, w, h )
-          S3.write( Content.thumbsBucket, urlPath + s, f )
-          S3.access( Content.thumbsBucket, urlPath + s, true )
+          if ( f != null ) {
+            S3.write( Content.thumbsBucket, urlPath + s, f )
+            S3.access( Content.thumbsBucket, urlPath + s, true )
+          }
         } finally {
           if ( f != null )
             f.delete
