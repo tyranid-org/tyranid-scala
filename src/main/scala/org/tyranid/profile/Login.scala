23aa602bf2b5910d5e1eca8fae72e7da32bac6e3
shindig <shindig@tyranid.org>
2012-09-21 13:59:43 -0500

    readers

--- src/main/scala/org/tyranid/profile/Login.scala
+++ src/main/scala/org/tyranid/profile/Login.scala
@@ -275,13 +275,16 @@ $( function() {
           else if ( email.isBlank ) 
             sess.warn( "Please log in." )
   
+            println( "WTF" )
           // check look and feel, and if rb, do json, otherwise this.
           T.LnF match {
              case LnF.RetailBrand =>
                val jsonRes = web.jsonRes( sess )
                
-               if ( !sess.hasErrors )
+               if ( !sess.hasErrors ) {
+                 println( redirect )
                  jsonRes.redirect = redirect.isBlank ? T.website | ( T.website + "/?l=" + redirect.encUrl )
+               }
                  
                web.json( jsonRes )
              case _ =>
