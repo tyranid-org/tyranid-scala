780fd6e4b29d203917dfc759ba1cac23dce628d2
Dimensia <dimensia@tyranid.org>
2012-10-30 16:42:11 -0500

    work

--- src/main/scala/org/tyranid/db/es/Es.scala
+++ src/main/scala/org/tyranid/db/es/Es.scala
@@ -139,7 +139,7 @@ object Es {
     )
 
   def search( query:Map[String,Any], user:User ):String = {
-    spam( "query=" + query.toJsonStr )
+    //sp am( "query=" + query.toJsonStr )
     ( Es.host + "/_search" ).POST( content = query.toJsonStr ).s
   }
 
