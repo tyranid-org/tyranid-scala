88b6798bb74dff576dbb1398889070f91eed7c4c
shindig <shindig@tyranid.org>
2012-12-28 13:44:59 -0600

    fix

--- src/main/scala/org/tyranid/document/zencoder/Zencoder.scala
+++ src/main/scala/org/tyranid/document/zencoder/Zencoder.scala
@@ -211,7 +211,10 @@ Zencoder-Api-Key: e834e2d2e415f7ef2303ecbb81ab54da
     if ( zid > 0 ) {
       //GET https://app.zencoder.com/api/v2/jobs/1234.xml?api_key=asdf1234
       val statusJson = Json.parse( Http.GET( "https://app.zencoder.com/api/v2/jobs/" + zid + ".json?api_key=" + apiKey ).s )
-      statusJson.s( 'state )
+      
+      val job = statusJson.get( "job" )
+      val state = job.s( 'state )
+      state
     } else {
       null
     }
