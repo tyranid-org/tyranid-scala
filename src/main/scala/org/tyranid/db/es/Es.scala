0b43061dde35e1c052f03e1017de8f6ca9591da2
Dimensia <dimensia@tyranid.org>
2013-01-25 13:46:30 -0600

    work

--- src/main/scala/org/tyranid/db/es/Es.scala
+++ src/main/scala/org/tyranid/db/es/Es.scala
@@ -94,7 +94,7 @@ class Indexer extends Actor {
       if ( json != "{}" ) {
 //sp am( "posting index=" + index + "  type=" + typ )
 //sp am( "url=" + Es.host + "/" + index + "/" + typ + "/" + id )
-spam( "json=" + json )
+//sp am( "json=" + json )
 
         val response  = ( Es.host + "/" + index + "/" + typ + "/" + id ).POST( content = json )
         val responseJson = Json.parse( response._s )
