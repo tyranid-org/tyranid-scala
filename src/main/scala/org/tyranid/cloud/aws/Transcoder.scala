c0b6e2b2503cdc71f837f136a3461fe086ab0bd1
shindig <shindig@tyranid.org>
2013-01-29 14:28:24 -0600

    added

---
+++ src/main/scala/org/tyranid/cloud/aws/Transcoder.scala
@@ -0,0 +1,8 @@
+package org.tyranid.cloud.aws
+
+class Transcoder {
+  val endpoint = "elastictranscoder.us-east-1.amazonaws.com"
+  val west_endpoint = "elastictranscoder.us-west-1.amazonaws.com"
+  val west_2_endpoint = "elastictranscoder.us-west-2.amazonaws.com"
+
+}
