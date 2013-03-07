703b194d91950be92b6e3d7fa34bcda958fb2880
shindig <shindig@tyranid.org>
2013-03-07 09:37:10 -0600

    put

--- src/main/scala/org/tyranid/math/Double.scala
+++ src/main/scala/org/tyranid/math/Double.scala
@@ -17,15 +17,22 @@
 
 package org.tyranid.math
 
-import org.tyranid.Imp._
-
 import scala.math
+
+import org.tyranid.Imp._
+import org.tyranid.time.Time
 
 object DoubleImp {
   
 }
 
 case class DoubleImp( d:Double ) {
+  val minutesFormat = "%02d:%02d" // mm:ss
+  val hoursFormat = "%02d:" + minutesFormat // hh:mm:ss
+  val daysFormat = "%02d:" + hoursFormat // dd:hh:mm:ss
+    
+  val OneDaySecs = 60 * 60 * 24
+  
   def toMoneyString = {
     String.format( "$%2.2f", double2Double( d ) )
   }
@@ -33,7 +40,22 @@ case class DoubleImp( d:Double ) {
   def toFormat( fmt:String ) = {
     String.format( fmt, double2Double( d ) )
   }
+  
+  def toTimeFormat = {
+    val days = ( d / OneDaySecs )._i
+    val hours = ( ( d % OneDaySecs ) / 3600 )._i
+    val minutes = ( ( d % 3600 ) / 60 )._i
+    val seconds = ( d % 60 )._i
+
+    if ( days > 0 )
+      daysFormat.format( days, hours, minutes, seconds )
+    else if ( hours > 0 )
+      hoursFormat.format( hours, minutes, seconds )
+    else 
+      minutesFormat.format( minutes, seconds )
+  }
 }
+
 
 
 
