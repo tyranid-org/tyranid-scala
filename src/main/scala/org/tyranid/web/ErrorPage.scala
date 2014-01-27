f53658354f779dbc4d08bb318652e99e558d736b
shindig <markb@dimensiology.com>
2014-01-27 12:34:41 -0600

    fix

--- src/main/scala/org/tyranid/web/ErrorPage.scala
+++ src/main/scala/org/tyranid/web/ErrorPage.scala
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -37,10 +37,10 @@ object Errorlet extends Weblet {
      "advice": { "reconnect": "none" }
    }
 ]""".getBytes
-    
-  def stopComet( web:WebContext ) = { 
+
+  def stopComet( web:WebContext ) = {
     web.res.setContentType( "application/json" )
-    
+
     try {
       new BufferedInputStream( new ByteArrayInputStream( stopCometStr ) ).transferTo( new BufferedOutputStream( web.res.getOutputStream() ), true )
     } catch {
@@ -48,10 +48,10 @@ object Errorlet extends Weblet {
       case e if e.getClass.getSimpleName == "EofException" =>
       case e2:Throwable => throw e2
     }
-    
+
     web.res.ok
   }
-        
+
   def handle( web:WebContext ) {
 
     rpath match {
@@ -71,48 +71,50 @@ object Errorlet extends Weblet {
       } else {
         web.forward( path = "/error/?full=1" )
       }
-      
+
     case "/404" =>
-      
+
       val originalUrl = web.req.getAttribute( "javax.servlet.forward.request_uri" )._s
+      if ( originalUrl.isBlank )
+        originalUrl = web.s( 'url ) or ""
 
       if ( T.session.ua( web ).isIE && ( originalUrl.startsWith( "/\"" ) || originalUrl.startsWith( "/%22/" ) ) ) {
         web.res.ok
         return
       }
-      
+
       println( originalUrl )
-      
+
       if ( originalUrl.startsWith( "/cometd" ) ) {
         stopComet( web )
         return
       }
-      
-      if ( originalUrl != null )
+
+      if ( originalUrl.notBlank )
         log( Event.Error404, "p" -> originalUrl )
-        
+
       if ( WebFilter.notAsset( originalUrl ) ) {
         if ( web.xhr ) {
           if ( web.b( 'full ) ) {
-            web.jsRes( JqHtml( "#main", 
+            web.jsRes( JqHtml( "#main",
       <div class="warning" style="margin:80px 0 0 200px; width:500px;">
        <img src="/images/warning.png" style="height:120px; width:120px; float:right;"/>
        <p style="font-size:32px;"><strong>404</strong></p>
-       <p>We're sorry, but the requested URL <pre>{ T.web.req.getAttribute( "javax.servlet.forward.request_uri" ) }</pre> was not found on our servers.</p>
+       <p>We're sorry, but the requested URL <pre>{ originalUrl }</pre> was not found on our servers.</p>
        <p>Please visit <a href={ "http://rbsupport.volerro.com/" }>{ "http://rbsupport.volerro.com/" }</a> if this is an urgent issue.</p>
        <p>Thank you.</p>
        <p><em>- The { B.applicationName } Team</em></p>
       </div> ) )
           } else {
             T.session.error(
-             "<p>We're sorry, but the requested URL <pre>" + T.web.req.getAttribute( "javax.servlet.forward.request_uri" )  + "</pre> was not found on our servers.</p>" +
+             "<p>We're sorry, but the requested URL <pre>" + originalUrl  + "</pre> was not found on our servers.</p>" +
              "<p>Please visit <a href='http://rbsupport.volerro.com'>http://rbsupport.volerro.com</a> if this is an urgent issue.</p>" +
              "<p>Thank you.</p>" +
              "<p><em>- The " + B.applicationName + " Team</em></p>".encJson
             )
-          }          
+          }
         } else {
-          T.web.forward( path = "/error/404?full=1" )
+          T.web.forward( js = "V.app.load('/error/404?full=1&url=" + originalUrl.encUrl + "')" )
         }
       }
     case "/throw" =>
